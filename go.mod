256c7651a13a7a6bfd7cfa4b10604fbe2bea89e4
vasilzhigilei <vasil.zhigilei@gmail.com>
2021-11-02 16:13:04 -0400

    Chi router creation and err checking function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gorest
 
 go 1.17
+
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
 func main() {
+	// ROUTER SETUP
+	r := chi.NewRouter()
 	
+	// Logs start/end, response status, and more of each request
+	r.Use(middleware.Logger)
+	// Timeout (60 second timeout)
+	r.Use(middleware.Timeout(60*time.Second))
+}
+
+func errCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
