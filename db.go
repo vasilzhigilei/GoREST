e9412043fd3dd909dbd32ff3319f9f757382f94e
vasilzhigilei <vasil.zhigilei@gmail.com>
2021-11-02 16:50:58 -0400

    handlers structures added

--- db.go
+++ db.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/jackc/pgx/v4"
+
+type Database struct {
+	conn *pgx.Conn
+}
--- handlers.go
+++ handlers.go
@@ -1,7 +1,46 @@
 package main
 
-import "github.com/jackc/pgx/v4"
+import "net/http"
 
-type Database struct {
-	conn *pgx.Conn
+type Customer struct {
+	ID uint `json:"id"`
+	Name string `json:"name"`
+	Password string `json:"password"`
+	Certificates []Certificate `json:certificates`
+}
+
+type Certificate struct {
+	ID uint `json:"id"`
+	Active bool `json:"active"`
+	PrivateKey string `json:"privatekey"`
+	Body string `json:"body"`
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func deleteCustomer(w http.ResponseWriter, r *http.Request){
+
+}
+
+func getCertificates(w http.ResponseWriter, r *http.Request){
+	ctx := r.Context()
+	customer_id, ok := ctx.Value("customer_id").(uint)
+	if !ok {
+		http.Error(w, http.StatusText(500), 500)
+	}
+	
+}
+
+func createCertificate(w http.ResponseWriter, r *http.Request){
+
+}
+
+func toggleCertificate(w http.ResponseWriter, r *http.Request){
+
+}
+
+func deleteCertificate(w http.ResponseWriter, r *http.Request){
+
 }
